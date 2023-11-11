87fc38c740c58912334d5e2dffba73e21eef5ea5
ilya <illyasedelnikov123@gmail.com>
2023-11-11 18:49:43 +0100

    v3. Added logging and code was refactored

--- src/main/java/com/example/todo_application/Controllers/TaskController.java
+++ src/main/java/com/example/todo_application/Controllers/TaskController.java
@@ -12,6 +12,7 @@ import com.example.todo_application.Repository.TaskRepository;
 import com.example.todo_application.dto.TaskDto;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,6 +25,7 @@ import java.util.stream.Collectors;
 @RequestMapping
 @Transactional
 @RequiredArgsConstructor
+@Slf4j
 public class TaskController {
 
     private final TaskRepository taskRepository;
@@ -44,6 +46,8 @@ public class TaskController {
             throw new TaskListNotFound("Task list does not contain any tasks");
         }
 
+        log.info("List Tasks have been obtained");
+
         return taskList
                 .getTasks()
                 .stream()
@@ -60,6 +64,8 @@ public class TaskController {
                 .stream()
                 .filter(anotherTask -> anotherTask.getId().equals(taskId))
                 .findFirst();
+
+        log.info("Task with id {} was obtained", taskId);
 
         return task
                 .map(taskEntity ->
@@ -80,12 +86,15 @@ public class TaskController {
                 .anyMatch(anotherTask -> anotherTask.getTitle().equals(task.getTitle()));
 
         if (titleExists) {
+            log.error("Task with {} title already exists", task.getTitle());
             throw new TaskWithRecourseExists(String.format("Task with %s title already exists", task.getTitle()));
         }
 
         taskList.addTaskToTaskList(task);
 
-        taskRepository.save(task);
+        taskRepository.saveAndFlush(task);
+
+        log.info("Task has been created successfully");
 
         return ResponseEntity.status(HttpStatus.CREATED).body(String.format("Task with id %s was successfully created",task.getId()));
 
@@ -94,6 +103,7 @@ public class TaskController {
     @PutMapping(UPDATE_TASK)
     public ResponseEntity<TaskDto> updateTask(@PathVariable Long taskListId, @PathVariable Long taskId, @RequestBody TaskEntity task) {
         if (task.getTitle().trim().isEmpty()) {
+            log.error("Task title cannot be empty");
             throw new BadRequestException("Task title cannot be empty");
         }
 
@@ -115,30 +125,25 @@ public class TaskController {
         taskEntity.setIsDone(task.getIsDone());
 
         taskList.updateTaskToTaskList(taskEntity);
-        taskRepository.save(taskEntity);
+        taskRepository.saveAndFlush(taskEntity);
+
+        log.info("Task has been updated successfully");
 
         return ResponseEntity.ok(taskDtoFactory.makeTaskDto(taskEntity));
     }
 
     @DeleteMapping(DELETE_TASK)
     public void deleteTask(@PathVariable Long taskId) {
-//        TaskListEntity taskList = controllerHelper.getTaskListOrThrowException(taskListId);
-//
-//        TaskEntity taskEntity = taskList.getTasks()
-//                .stream()
-//                .filter(anotherTask -> anotherTask.getId().equals(taskId))
-//                .findFirst()
-//                .orElseThrow(
-//                        () -> new TaskNotFoundException(String.format("Task with id %s was not found", taskId))
-//                );
         boolean exists = taskRepository.existsById(taskId);
 
         if (exists) {
+            log.error("Task does with id {} not exist", taskId);
             taskRepository.deleteById(taskId);
 
         } else {
+            log.info("Task has been deleted");
             throw new TaskNotFoundException("task was not found");
         }
-//        taskList.deleteTaskFromTaskList(taskEntity);
     }
 }
+
--- src/main/java/com/example/todo_application/Controllers/TaskListController.java
+++ src/main/java/com/example/todo_application/Controllers/TaskListController.java
@@ -3,14 +3,12 @@ package com.example.todo_application.Controllers;
 import com.example.todo_application.Entity.TaskListEntity;
 import com.example.todo_application.Exception.TaskListNotFound;
 import com.example.todo_application.Exception.TaskListWithRecourseExists;
-import com.example.todo_application.Factory.TaskDtoFactory;
 import com.example.todo_application.Factory.TaskListDtoFactory;
 import com.example.todo_application.Repository.TaskListRepository;
 import com.example.todo_application.dto.TaskListDto;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,6 +20,7 @@ import java.util.stream.Collectors;
 @RequestMapping("/api")
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class TaskListController {
     private final TaskListRepository taskListRepository;
     private final TaskListDtoFactory taskListDtoFactory;
@@ -35,6 +34,9 @@ public class TaskListController {
 
     @GetMapping(GET_TASKS_LIST)
     public List<TaskListDto> getTasksList() {
+
+        log.info("Task List has been obtained");
+
         return taskListRepository.findAll()
                 .stream()
                 .map(taskListDtoFactory::makeListDto)
@@ -43,6 +45,9 @@ public class TaskListController {
 
     @GetMapping(GET_TASK_LIST_BY_ID)
     public TaskListDto getTaskById(@PathVariable Long id) {
+
+        log.info("Task List with id {} has been obtained", id);
+
         return taskListRepository.findById(id)
                 .map(taskListDtoFactory::makeListDto)
                 .orElseThrow(
@@ -53,8 +58,11 @@ public class TaskListController {
     @PostMapping(CREATE_TASK_LIST)
     public ResponseEntity<String> createTaskList(@RequestBody TaskListEntity taskList) {
         if (taskListRepository.existsByName(taskList.getName())) {
+            log.error("Task cannot contain name that exists");
             throw new TaskListWithRecourseExists(String.format("Task list with name %s already exists", taskList.getName()));
         }
+
+        log.info("Task has been created");
 
         TaskListEntity savedTaskListEntity = taskListRepository.save(taskList);
         return ResponseEntity.status(HttpStatus.CREATED).body(String.format("Task list with id %s was created", savedTaskListEntity.getId()));
@@ -70,8 +78,10 @@ public class TaskListController {
         foundTaskListEntity.setName(taskList.getName());
         foundTaskListEntity.setTasks(taskList.getTasks());
 
-        TaskListEntity updatedTaskList = taskListRepository.save(foundTaskListEntity);
+        TaskListEntity updatedTaskList = taskListRepository.saveAndFlush(foundTaskListEntity);
         TaskListDto taskListDto = taskListDtoFactory.makeListDto(updatedTaskList);
+
+        log.info("Task with id {} has been updated", id);
 
         return ResponseEntity.ok(taskListDto);
     }
@@ -79,10 +89,13 @@ public class TaskListController {
     @DeleteMapping(DELETE_TASK_LIST_BY_ID)
     public ResponseEntity<String> deleteTaskListById(@PathVariable Long id) {
         if (taskListRepository.findById(id).isEmpty()) {
+            log.error("Task with id {} does not exist", id);
             throw new TaskListNotFound(String.format("Task list with id %s was not found", id));
         }
 
         taskListRepository.deleteById(id);
+
+        log.info("Task with id {} has been deleted", id);
 
         return ResponseEntity.status(HttpStatus.OK).body(String.format("Task list with id %s was deleted", id));
     }
