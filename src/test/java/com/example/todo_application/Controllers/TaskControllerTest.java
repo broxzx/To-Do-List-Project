5cb793b641565880590324565086bf64dfc7adf0
ilya <illyasedelnikov123@gmail.com>
2023-11-22 18:36:01 +0100

    v5. Added every tests for method in TaskController.

--- src/test/java/com/example/todo_application/Controllers/TaskControllerTest.java
+++ src/test/java/com/example/todo_application/Controllers/TaskControllerTest.java
@@ -1,13 +1,8 @@
 package com.example.todo_application.Controllers;
 
-import com.example.todo_application.Controllers.TaskController;
 import com.example.todo_application.Controllers.helper.ControllerHelper;
 import com.example.todo_application.Entity.TaskEntity;
 import com.example.todo_application.Entity.TaskListEntity;
-import com.example.todo_application.Exception.BadRequestException;
-import com.example.todo_application.Exception.TaskListNotFound;
-import com.example.todo_application.Exception.TaskNotFoundException;
-import com.example.todo_application.Exception.TaskWithRecourseExists;
 import com.example.todo_application.Factory.TaskDtoFactory;
 import com.example.todo_application.Repository.TaskRepository;
 import com.example.todo_application.dto.TaskDto;
@@ -21,8 +16,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -50,8 +45,10 @@ public class TaskControllerTest {
     @DisplayName("GET /api/{taskId}/task/ returns all tasks that TaskListEntity contains")
     void testGetAllTasks() {
         Long taskId = 1L;
+
         TaskListEntity taskList = new TaskListEntity();
         List<TaskEntity> tasks = new ArrayList<>();
+
         tasks.add(new TaskEntity(1L, "Task 1", "Description 1", null, false, taskList));
         tasks.add(new TaskEntity(2L, "Task 2", "Description 2", null, false, taskList));
         taskList.setTasks(tasks);
@@ -69,19 +66,89 @@ public class TaskControllerTest {
     }
 
     @Test
-    void testGetTaskDtoById() {
+    void testGetTaskById() {
+        Long taskListId = 1L;
+        Long taskId = 2L;
 
+        TaskListEntity taskListMock = mock(TaskListEntity.class);
+        TaskEntity taskEntity = new TaskEntity(taskId, "Task 1", "Description 1", null, false, taskListMock);
+
+        when(controllerHelper.getTaskListOrThrowException(taskListId)).thenReturn(taskListMock);
+        when(taskDtoFactory.makeTaskDto(any())).thenReturn(new TaskDto());
+
+        when(taskListMock.getTasks()).thenReturn(Collections.singletonList(taskEntity));
+
+        ResponseEntity<TaskDto> result = taskController.getTaskDtoById(taskListId, taskId);
+
+        assertNotNull(result);
+        assertTrue(result.getStatusCode().is2xxSuccessful());
+        verify(controllerHelper, times(1)).getTaskListOrThrowException(taskListId);
+        verify(taskDtoFactory, times(1)).makeTaskDto(any());
     }
 
     @Test
     void testCreateTask() {
+        Long taskListId = 1L;
+        Long taskId = 1L;
+
+        TaskListEntity taskListMock = mock(TaskListEntity.class);
+        TaskEntity taskEntity = new TaskEntity(taskId, "Task 1", "Description 1", null, false, taskListMock);
+
+        taskListMock.setTasks(List.of(taskEntity));
+
+        when(controllerHelper.getTaskListOrThrowException(taskListId)).thenReturn(taskListMock);
+        when(taskListMock.getTasks()).thenReturn(Collections.emptyList());
+        when(taskRepository.saveAndFlush(any())).thenReturn(taskEntity);
+
+        ResponseEntity<String> res = taskController.createTask(taskListId, taskEntity);
+
+        assertNotNull(res);
+        assertTrue(res.getStatusCode().is2xxSuccessful());
+        verify(controllerHelper, times(1)).getTaskListOrThrowException(taskListId);
+        verify(taskRepository, times(1)).saveAndFlush(taskEntity);
     }
 
     @Test
     void testUpdateTask() {
+        Long taskListId = 1L;
+        Long taskId = 1L;
+
+        TaskListEntity taskListMock = mock(TaskListEntity.class);
+        TaskEntity existedEntity = new TaskEntity(taskId, "Task 1", "Description 1", null, false, taskListMock);
+        TaskEntity updatedEntity = new TaskEntity(taskId, "Updated Task", "Updated Description", null, true, taskListMock);
+
+        when(controllerHelper.getTaskListOrThrowException(taskListId)).thenReturn(taskListMock);
+        when(taskRepository.existsById(taskId)).thenReturn(true);
+        when(taskListMock.getTasks()).thenReturn(Collections.singletonList(existedEntity));
+        when(taskRepository.saveAndFlush(any(TaskEntity.class))).thenReturn(updatedEntity);
+
+        ResponseEntity<TaskDto> result = taskController.updateTask(taskListId, taskId, updatedEntity);
+
+        assertNotNull(result);
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+        assertNotNull(result.getBody(), "Response body should not be null");
+        assertTrue(result.getStatusCode().is2xxSuccessful());
+        TaskDto updatedDto = result.getBody();
+        System.out.println("Updated Dto: " + updatedDto);
+        assertNotNull(updatedDto);
+        assertEquals("Updated Task", updatedDto.getTitle());
+        assertEquals("Updated Description", updatedDto.getDescription());
+        assertTrue(updatedDto.getIsDone());
+
+        verify(controllerHelper, times(1)).getTaskListOrThrowException(taskListId);
+        verify(taskRepository, times(1)).existsById(taskId);
+        verify(taskRepository, times(1)).saveAndFlush(any(TaskEntity.class));
     }
 
     @Test
     void testDeleteTask() {
+        Long taskId = 1L;
+
+        when(taskRepository.existsById(taskId)).thenReturn(true);
+
+        taskController.deleteTask(taskId);
+
+        verify(taskRepository, times(1)).existsById(taskId);
+        verify(taskRepository, times(1)).deleteById(taskId);
     }
 }
